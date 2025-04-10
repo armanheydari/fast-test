738d4aab7dea07ad788e2a617f8bc4d24d7c6fe5
armanheydari <Rmanheydari2000@gmail.com>
2025-04-10 17:44:37 -0600

    initialize simple app to deploy

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Marketing(BaseModel):
+    campaign_name: str
+    finance: int
+    conversion_rate: float
+
+
+@app.get("/")
+async def root():
+    return {"Arman": "Welcome to application"}
+
+
+@app.get("/marketing_data")
+async def marketing():
+    response = Marketing(
+        campaign_name="Mozhan",
+        finance=26,
+        conversion_rate=1.2
+    )
+    return response
+
+
+@app.get("/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
